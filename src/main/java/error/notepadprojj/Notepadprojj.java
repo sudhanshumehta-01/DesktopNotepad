50bdc7d0fbc9fe2492a0d5ad2a58a5cd65c3363a
sudhanshumehta-01 <166909273+sudhanshumehta-01@users.noreply.github.com>
2025-07-12 17:34:23 +0530

    Initial commit

---
+++ src/main/java/error/notepadprojj/Notepadprojj.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package error.notepadprojj;
+
+/**
+ *
+ * @author sudha
+ */
+public class Notepadprojj {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/error/notepadprojj/printer.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package error.notepadprojj;
+
+import java.awt.print.PageFormat;
+
+/**
+ *
+ * @author sudha
+ */
+class printer {
+
+    static PageFormat defaultPage() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void printDialog() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
